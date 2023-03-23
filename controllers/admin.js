27ddc46886e986b56a9538ce90dac77fc23992e7
Ujjawal Kumar <ujjawalrr@gmail.com>
2023-03-23 18:09:29 +0530

    admin

--- controllers/admin.js
+++ controllers/admin.js
@@ -3,7 +3,6 @@ import User from "../models/User.js";
 export const getAllUsers = async (req,res,next)=>{
     try {
       const user = await User.find({}, { _id:0 })
-      console.log(user)
       res.status(200).json(user);
     } catch (err) {
       next(err);
